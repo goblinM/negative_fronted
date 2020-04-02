3e52a6cbed94e9b0ceb9fa0c857fd9bf2351ba85
mominxin <mominxin@polyv.net>
2020-04-02 14:44:33 +0800

    取消main.js请求拦截

--- src/main.js
+++ src/main.js
@@ -50,133 +50,13 @@ Vue.prototype.getCookie = function (name) {
   //如果cookies存在
   if (start > -1){
     var end = document.cookie.indexOf(";",name);
-    if(end == -1){
+    if(end === -1){
       end = document.cookie.length;
     }
     value = document.cookie.substring(start+name.length,end);
   }
   return value;
-}
-// 超时时间
-axios.defaults.timeout = 600000;
-
-//设置请求拦截
-var loadinginstace;
-Vue.prototype.$requestInterceptor = axios.interceptors.request.use(
-    config => {
-      loadinginstace = Loading.service({ fullscreen: true ,background:"rgba(0,0,0,0)"});
-
-      // console.log(sessionStorage.hasOwnProperty("token") && sessionStorage.hasOwnProperty("now_user_id"))
-      if(router.currentRoute.name !='login'){
-        config.headers= {
-            'Content-Type':'application/x-www-form-urlencoded',
-            'Authorization':sessionStorage.getItem("token"),
-            // csrfmiddlewaretoken:this.getCookie("csrftoken")
-          };
-        /****************************这里是判断token是否存在不存在则返回登录界面start********************************/
-        // if(sessionStorage.hasOwnProperty("token") && sessionStorage.hasOwnProperty("now_user_id")) {
-        //   //console.log(sessionStorage.getItem("token"));
-        //   config.headers= {
-        //     'Content-Type':'application/x-www-form-urlencoded',
-        //     'Authorization':sessionStorage.getItem("token"),
-        //     // csrfmiddlewaretoken:this.getCookie("csrftoken")
-        //   };
-        // }else {
-        //
-        //   console.log('sessionStorage没登录token');
-        //   router.push({name:'login',query:{redirect: decodeURIComponent(router.currentRoute.fullPath)}})
-        //
-        // }
-        /****************************这里是判断token是否存在不存在则返回登录界面end********************************/
-      }
-                return config;
-    },
-    err => {
-      Message.error({
-        message: '加载超时'
-      })
-        return Promise.reject(err);
-    });
-
-// http响应拦截器
-// axios.interceptors.response.forEach(function (e) {
-//   console.log(e)
-// })
-Vue.prototype.$responseInterceptor = axios.interceptors.response.use(data => {
-  // 响应成功关闭loading
-
-  loadinginstace.close();
-  return data
-}, error => {
-  loadinginstace.close();
-
-  if (error.response) {
-    switch (error.response.status) {
-      case 401:
-        // 这里写清除token的代码
-        if(router.currentRoute.name !='login'){
-          sessionStorage.removeItem("token");
-          router.push({name:'login',query:{redirect: decodeURIComponent(router.currentRoute.fullPath)}})
-        }
-        break;
-      default:
-        Message.info({
-          message: '加载失败'
-        });
-
-    }
-  }
-  return Promise.reject(error)
-});
-
-// router.beforeEach((to, from, next) => {
-//   if (!to.meta.requireAuth) {  // 判断该路由是否需要登录权限
-//     if (sessionStorage.hasOwnProperty("token")) {  // 通过vuex state获取当前的token是否存在
-//       next();
-//     }
-//     else {
-//       next({
-//         path: '/login',
-//         query: {redirect: to.fullPath}  // 将跳转的路由path作为参数，登录成功后跳转到该路由
-//       })
-//     }
-//   }
-//   else {
-//     next();
-//   }
-// })
-Vue.component('remote-script', {
-
-    render: function (createElement) {
-        var self = this;
-        return createElement('script', {
-            attrs: {
-                type: 'text/javascript',
-                src: this.src
-            },
-            on: {
-                load: function (event) {
-                    self.$emit('load', event);
-                },
-                error: function (event) {
-                    self.$emit('error', event);
-                },
-                readystatechange: function (event) {
-                    if (this.readyState == 'complete') {
-                        self.$emit('load', event);
-                    }
-                }
-            }
-        });
-    },
-
-    props: {
-        src: {
-            type: String,
-            required: true
-        }
-    }
-});
+};
 
 new Vue({
   el: '#app',
@@ -184,4 +64,4 @@ new Vue({
   // store,
   components: { App },
   template: '<App/>'
-})
+});
